f7de0777c8a6d10af38c665e966abfeb40179a92
n8toons <77544350+n8toons@users.noreply.github.com>
2021-05-11 16:17:42 -0300

    Add files via upload

---
+++ roomfinder.js
@@ -0,0 +1,10 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyCizzfpZxjDA3_2yanQGPgViFt2WUbWdF4",
+    authDomain: "school-chat-app-on-vcs.firebaseapp.com",
+    databaseURL: "https://school-chat-app-on-vcs-default-rtdb.firebaseio.com",
+    projectId: "school-chat-app-on-vcs",
+    storageBucket: "school-chat-app-on-vcs.appspot.com",
+    messagingSenderId: "992098832431",
+    appId: "1:992098832431:web:0654d0480f5a5014b1f6da",
+    measurementId: "G-14DEBF0J9P"
+  };
